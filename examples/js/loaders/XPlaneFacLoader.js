9340bb92360551c3afceae62376adb32f9d3fdd9
aussig <austin@opensceneryx.com>
2019-08-08 09:05:32 +0100

    WIP on implementation of X-Plane facade loader, partially working.

--- examples/js/loaders/XPlaneFacLoader.js
+++ examples/js/loaders/XPlaneFacLoader.js
@@ -1,6 +1,7 @@
 /**
  * @author aussi / https://github.com/aussig
  * Heavily derived from OBJLoader @author mrdoob / http://mrdoob.com/
+ * And from xptools @author Laminar Research
  */
 
 THREE.XPlaneFacLoader = ( function () {
@@ -350,11 +351,8 @@ THREE.XPlaneFacLoader = ( function () {
 
 			var container = new THREE.Group();
 
-			// Create underlying surface to provide contrast for our lines
-			var geometry = new THREE.BoxGeometry( 1.1, 0.01, 1.1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x008000 } );
-			var plane = new THREE.Mesh( geometry, material );
-			container.add( plane );
+			// Add facade
+			this.addFacade( container, walls, roofUV, scaleX, scaleY, floorsMin, floorsMax, ring, roof );
 
 			console.timeEnd( 'XPlaneFacLoader' );
 
@@ -362,50 +360,109 @@ THREE.XPlaneFacLoader = ( function () {
 
 		},
 
-		addTree: function ( container, treeData, index, spacingX, spacingZ, randomX, randomZ, scaleX, scaleY ) {
-			// treeData: <s> <t> <w> <y> <offset> <frequency> <min height> <max height> <quads> <type>
-			var treeH = treeData[6] + Math.random() * ( treeData[7] - treeData[6] );
-			var treeW = treeH * treeData[2] / treeData[3];
-			var treeX = ( index % treesPerRow ) * spacingX + randomX * ( 2.0 * Math.random() - 1.0 );
-			var treeZ = ( index / treesPerRow ) * spacingZ + randomZ * ( 2.0 * Math.random() - 1.0 );
-			var treeRotation = Math.random();
+		addFacade: function ( container, walls, roofUV, scaleX, scaleY, floorsMin, floorsMax, ring, roof ) {
+			var wantLen = 3;               // minimum size to draw as initial guess
+			var wantFloors = 1;            // # floors in the way the Obj8 is constructed. Not the floors of the building/facade represents.
+			var hasMiddles = false;        // if there are no middle sections in the walls, the facade will allow only one floor, i.e. is fixed height
+			var quads;
 
 			var vertices = [];
 			var normals = [];
 			var uvs = [];
 
+			var vertexList = [];
+			var uvList = [];
+
 			var scope = this;
 
-			for ( var i = 0; i < treeData[8]; i++ ) {
-				var quadRotation = Math.PI * ( treeRotation + i / treeData[8] );        // tree rotation
-				var quadX = treeW * Math.sin( quadRotation );
-				var quadZ = treeW * Math.cos( quadRotation );
+			if (walls.length == 0) return true;
 
-				// Tri 1, point 1
-				vertices.push( treeX - quadX * ( treeData[4] / treeData[2] ), 0.0, treeZ - quadZ * ( treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( treeData[0] / scaleX, treeData[1] / scaleY );
-				// Tri 1, point 2
-				vertices.push( treeX + quadX * ( 1.0 - treeData[4] / treeData[2] ), 0.0, treeZ + quadZ * ( 1.0 - treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( ( treeData[0] + treeData[2] ) / scaleX, treeData[1] / scaleY );
-				// Tri 1, point 3
-				vertices.push( treeX + quadX * ( 1.0 - treeData[4] / treeData[2] ), treeH, treeZ + quadZ * ( 1.0 - treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( ( treeData[0] + treeData[2] ) / scaleX, ( treeData[1] + treeData[3] ) / scaleY );
+			// Sanitise walls
+			for (var i = 0; i < walls.length; i++) {
+				if (walls[ i ].vert[ 1 ] == 0.0)
+					break;
 
-				// Tri 2, point 1
-				vertices.push( treeX - quadX * ( treeData[4] / treeData[2] ), 0.0, treeZ - quadZ * ( treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( treeData[0] / scaleX, treeData[1] / scaleY );
-				// Tri 2, point 3
-				vertices.push( treeX + quadX * ( 1.0 - treeData[4] / treeData[2] ), treeH, treeZ + quadZ * ( 1.0 - treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( ( treeData[0] + treeData[2] ) / scaleX, ( treeData[1] + treeData[3] ) / scaleY );
-				// Tri 2, point 4
-				vertices.push( treeX - quadX * (treeData[4] / treeData[2] ), treeH, treeZ - quadZ * ( treeData[4] / treeData[2] ) );
-				normals.push( 0.0, 1.0, 0.0 );
-				uvs.push( treeData[0] / scaleX, ( treeData[1] + treeData[3] ) / scaleY );
+				if (walls[ i ].vert[ 2 ] == 0.0)
+					walls[ i ].vert[ 2 ] = walls[ i ].vert[ 1 ];
+				else
+					hasMiddles = true;
+
+				if (walls[ i ].vert[ 3 ] == 0.0)
+					walls[ i ].vert[ 3 ] = walls[ i ].vert[ 2 ];
+				else
+					hasMiddles = true;
+
+				if (walls[ i ].hori[ 2 ] == 0.0) {
+					walls[ i ].hori[ 2 ] = walls[ i ].hori[ 1 ];
+					walls[ i ].hori[ 1 ] = walls[ i ].hori[ 0 ];
+				}
+
+				if (walls[ i ].hori[ 3 ] == 0.0)
+					walls[ i ].hori[ 3 ] = walls[ i ].hori[ 2 ];
+
+				var idealLen = (walls[ i ].hori[ 2 ] - walls[ i ].hori[ 0 ] + walls[ i ].hori[ 3 ] - walls[ i ].hori[ 1 ]) * scaleX;
+				wantLen = Math.max( wantLen, idealLen );
+			}
+
+			if (hasMiddles)
+				wantFloors = 2;
+			else {
+				floorsMin = -1.0;
+				floorsMax = (walls[ 0 ].vert[ 2 ] - walls[ 0 ].vert[ 0 ]) * scaleY;
+			}
+
+			var x = 0.0;
+			var y = 0.0;
+			var z = 0.0;
+
+			for (var level = 0; level < wantFloors; level++) {
+				quads = 0;
+
+				for (var fl = 0; fl <= 1; fl++) {
+					x = z = 0.0;
+
+					if (fl) y += (walls[ 0 ].vert[ level + 2 ] - walls[ 0 ].vert[ level ]) * scaleY;   // height of each floor
+
+					for (var i = 0; i <= 1; i++) {
+						for (var j = 0; j <= 1; j++) {
+							if (!ring && i && j) break;      // open facades (fences etc) drawn with 3 sides only
+
+							// wall selection. We want to show off as many different walls as practical
+							var w = 2 * i + j;
+							if (w >= walls.length) w = 0;   // show default wall if we run out of varieties
+
+							var t = walls[ w ].vert[ 2 * fl + level ];
+							var lenLeft = wantLen - (walls[ w ].hori[ 2 ] - walls[ w ].hori[ 0 ] + walls[ w ].hori[ 3 ] - walls[ w ].hori[ 1 ]) * scaleX;
+							var exact = 2.0 + lenLeft / ((walls[ w ].hori[ 2 ] - walls[ w ].hori[ 1 ]) * scaleX);
+							var sects = Math.ceil(exact);
+
+							for (var k = 0; k < sects; k++) {
+								var s1 = walls[ w ].hori[ Math.min( k, 1 ) ];
+								var s2 = walls[ w ].hori[ 2 + (k == sects - 1) ];
+								var dx = (s2 - s1) * scaleX;
+
+								vertexList.push( [ x, y, z ] );
+								uvList.push( [ s1, t ] );
+
+								if (j == 0) z += (1 - 2 * i) * dx * (1.0 - (k == sects - 2) * (sects - exact));
+								else x += (1 - 2 * i) * dx * (1.0 - (k == sects - 2) * (sects - exact));
+
+								vertexList.push( [ x, y, z ] );
+								uvList.push( [ s2 - (s2 - s1) * (k == sects - 2) * (sects - exact), t ] );
+
+								if (fl) quads++;
+							}
+						}
+					}
+				}
+
+				var seq = [ 0, 1, 2 * quads, 1, 2 * quads + 1, 2 * quads ];
+				for (var i = 0; i < 6 * quads; i++) {
+					var vertexIndex = 2 * Math.floor(i / 6) + seq[ i % 6 ];
+					vertices.push( vertexList[ vertexIndex ][ 0 ], vertexList[ vertexIndex ][ 1 ], vertexList[ vertexIndex ][ 2 ] );
+					normals.push( 0.0, 1.0, 0.0 );
+					uvs.push( uvList[ vertexIndex ][ 0 ], uvList[ vertexIndex ][ 1 ] );
+				}
 			}
 
 			var geometry = new THREE.BufferGeometry( );
@@ -413,10 +470,60 @@ THREE.XPlaneFacLoader = ( function () {
 			geometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
 			geometry.addAttribute( 'uv', new THREE.Float32BufferAttribute( uvs, 2 ) );
 
-			var polygon = new THREE.Mesh( geometry, scope.roofMaterial );
+			var polygon = new THREE.Mesh( geometry, scope.wallMaterial );
 			container.add( polygon );
-		}
+/*
+			vertices = [];
+			normals = [];
+			uvs = [];
 
+			vertexList = [];
+			uvList = [];
+
+			if (roof) {
+				pt[1] = obj->xyz_max[1]; // height
+
+				for (int i = 0; i<2; ++i)
+					for (int j = 0; j<2; ++j)
+					{
+						pt[0] = i ? obj->xyz_min[0] : obj->xyz_max[0];
+						pt[2] = j ? obj->xyz_min[2] : obj->xyz_max[2];
+
+						pt[6] = roof_uv[2*i];
+						pt[7] = roof_uv[1+2*j];
+						r_obj->geo_tri.append(pt);
+					}
+
+				r_obj->geo_tri.get_minmax(r_obj->xyz_min,r_obj->xyz_max);
+
+				// "IDX "
+				int seq[6] = {0, 1, 2*quads, 1, 2*quads+1, 2*quads};
+				for (int i = 0; i < 6*quads; ++i)
+					r_obj->indices.push_back(2*(i/6)+seq[i%6]);
+
+				// "ATTR_LOD"
+				r_obj->lods.push_back(XObjLOD8());
+				r_obj->lods.back().lod_near = 0;
+				r_obj->lods.back().lod_far  = 1000;
+
+				// "TRIS ";
+				cmd.cmd = obj8_Tris;
+				cmd.idx_offset = 0;
+				cmd.idx_count  = r_obj->indices.size();
+				r_obj->lods.back().cmds.push_back(cmd);
+
+				info.previews.push_back(r_obj);
+			}
+
+			geometry = new THREE.BufferGeometry( );
+			geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+			geometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
+			geometry.addAttribute( 'uv', new THREE.Float32BufferAttribute( uvs, 2 ) );
+
+			polygon = new THREE.Mesh( geometry, scope.roofMaterial );
+			container.add( polygon );
+*/
+		}
 	};
 
 	return XPlaneFacLoader;
